75020ca7cf5b8f17559b14a0ac2bbf854e47138c
Naresh Babu <nareshvvh172020@gmail.com>
2025-05-31 13:54:41 +0530

    Fix: CORS settings for Vercel

--- exam/settings.py
+++ exam/settings.py
@@ -44,12 +44,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://angular-crud-frontend-psi.vercel.app/' 
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
